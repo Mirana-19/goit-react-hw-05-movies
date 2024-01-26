13390f6c8de0be8f99129dc5244337324af82e24
Marta Żak <martysik19@gmail.com>
2024-01-26 07:43:39 +0100

    adds styles to Cast component

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieCast } from 'services/api';
 import img from '../img/defaultImg.jpg';
+import { CastImg, CastItemStyled, CastStyled } from './Cast.styled';
 
 export default function Cast() {
   const [cast, setCast] = useState([]);
@@ -16,14 +17,19 @@ export default function Cast() {
   const imgPath = 'https://image.tmdb.org/t/p/w500/';
 
   return (
-    <ul>
-      {cast.map(({ character, name, profile_path }) => (
-        <li key={name}>
-          <img src={profile_path ? imgPath + profile_path : img} alt="name" />
-          <h3>{name}</h3>
-          <p>Character: {character}</p>
-        </li>
-      ))}
-    </ul>
+    <CastStyled>
+      {cast.length &&
+        cast.map(({ character, name, profile_path }) => (
+          <CastItemStyled key={name}>
+            <CastImg
+              src={profile_path ? imgPath + profile_path : img}
+              alt="name"
+            />
+            <h3>{name}</h3>
+            <p>Character: {character}</p>
+          </CastItemStyled>
+        ))}
+      {!cast.length && <p>Sorry, there is no information about the cast.</p>}
+    </CastStyled>
   );
 }
---
+++ src/components/Cast/Cast.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const CastStyled = styled.ul`
+  display: flex;
+
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+export const CastItemStyled = styled.li`
+  flex-basis: 10%;
+`;
+
+export const CastImg = styled.img`
+  height: 200px;
+  border-radius: 12px;
+`;
